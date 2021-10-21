2f6f7795bf4589c539859f8d1668c6c5a7d5b525
Court Schuett <schuettc@amazon.com>
2021-10-21 09:42:41 -0500

    formatting and descoping permissions

--- src/createChimeSMAResources/createChimeSMAResources.py
+++ src/createChimeSMAResources/createChimeSMAResources.py
@@ -1,7 +1,7 @@
 import boto3
 import time
 
-chime = boto3.client('chime')
+chime = boto3.client("chime")
 
 
 def get_phone_number():
@@ -9,110 +9,102 @@ def get_phone_number():
         # AreaCode='string',
         # City='string',
         # Country='string',
-        State='AZ',
+        State="AZ",
         # TollFreePrefix='string',
-        MaxResults=1
+        MaxResults=1,
     )
-    phone_number_to_order = search_response['E164PhoneNumbers'][0]
-    print('Phone Number: ' + phone_number_to_order)
+    phone_number_to_order = search_response["E164PhoneNumbers"][0]
+    print("Phone Number: " + phone_number_to_order)
     phone_order = chime.create_phone_number_order(
-        ProductType='SipMediaApplicationDialIn',
+        ProductType="SipMediaApplicationDialIn",
         E164PhoneNumbers=[
             phone_number_to_order,
-        ]
+        ],
     )
-    print('Phone Order: ' + str(phone_order))
+    print("Phone Order: " + str(phone_order))
 
     check_phone_order = chime.get_phone_number_order(
-        PhoneNumberOrderId=phone_order['PhoneNumberOrder']['PhoneNumberOrderId']
+        PhoneNumberOrderId=phone_order["PhoneNumberOrder"]["PhoneNumberOrderId"]
     )
-    order_status = check_phone_order['PhoneNumberOrder']['Status']
+    order_status = check_phone_order["PhoneNumberOrder"]["Status"]
     timeout = 0
 
-    while not order_status == 'Successful':
+    while not order_status == "Successful":
         timeout += 1
-        print('Checking status: ' + str(order_status))
+        print("Checking status: " + str(order_status))
         time.sleep(5)
         check_phone_order = chime.get_phone_number_order(
-            PhoneNumberOrderId=phone_order['PhoneNumberOrder']['PhoneNumberOrderId']
+            PhoneNumberOrderId=phone_order["PhoneNumberOrder"]["PhoneNumberOrderId"]
         )
-        order_status = check_phone_order['PhoneNumberOrder']['Status']
-        if order_status == 'Failed':
-            raise Exception(
-                'Order number failed: {}'.format(check_phone_order))
+        order_status = check_phone_order["PhoneNumberOrder"]["Status"]
+        if order_status == "Failed":
+            raise Exception("Order number failed: {}".format(check_phone_order))
         if timeout == 5:
-            return 'Could not get phone number: {}'.format(check_phone_order)
+            return "Could not get phone number: {}".format(check_phone_order)
     return phone_number_to_order
 
 
 def create_SMA(region, name, lambdaArn):
     sma_create_response = chime.create_sip_media_application(
         AwsRegion=region,
-        Name=name+'-SMA',
+        Name=name + "-SMA",
         Endpoints=[
-            {
-                'LambdaArn': lambdaArn
-            },
-        ]
+            {"LambdaArn": lambdaArn},
+        ],
     )
-    print('sma create: ' + str(sma_create_response))
-    return sma_create_response['SipMediaApplication']['SipMediaApplicationId']
+    print("sma create: " + str(sma_create_response))
+    return sma_create_response["SipMediaApplication"]["SipMediaApplicationId"]
 
 
 def create_sip_rule(name, phone_number, sma_ID, region):
     print(phone_number)
     sip_rule_response = chime.create_sip_rule(
         Name=name,
-        TriggerType='ToPhoneNumber',
+        TriggerType="ToPhoneNumber",
         TriggerValue=phone_number,
         Disabled=False,
         TargetApplications=[
-            {
-                'SipMediaApplicationId': sma_ID,
-                'Priority': 1,
-                'AwsRegion': region
-            },
-        ]
+            {"SipMediaApplicationId": sma_ID, "Priority": 1, "AwsRegion": region},
+        ],
     )
-    print('sip rule response: ' + str(sip_rule_response))
+    print("sip rule response: " + str(sip_rule_response))
     return sip_rule_response
 
 
 def on_event(event, context):
     print(event)
-    request_type = event['RequestType']
-    if request_type == 'Create':
+    request_type = event["RequestType"]
+    if request_type == "Create":
         return on_create(event)
-    if request_type == 'Update':
+    if request_type == "Update":
         return on_update(event)
-    if request_type == 'Delete':
+    if request_type == "Delete":
         return on_delete(event)
     raise Exception("Invalid request type: %s" % request_type)
 
 
 def on_create(event):
-    physical_id = 'smaResources'
-    region = event['ResourceProperties']['region']
-    name = event['ResourceProperties']['smaName']
-    lambdaArn = event['ResourceProperties']['lambdaArn']
+    physical_id = "smaResources"
+    region = event["ResourceProperties"]["region"]
+    name = event["ResourceProperties"]["smaName"]
+    lambdaArn = event["ResourceProperties"]["lambdaArn"]
 
     new_phone_number = get_phone_number()
     sma_ID = create_SMA(region, name, lambdaArn)
     rule_name = str(new_phone_number)
-    sip_rule_response = create_sip_rule(
-        rule_name, new_phone_number, sma_ID, region)
-    create_SMA_response = {'smaID': sma_ID, 'phoneNumber': new_phone_number}
-    return {'PhysicalResourceId': physical_id, 'Data': create_SMA_response}
+    sip_rule_response = create_sip_rule(rule_name, new_phone_number, sma_ID, region)
+    create_SMA_response = {"smaID": sma_ID, "phoneNumber": new_phone_number}
+    return {"PhysicalResourceId": physical_id, "Data": create_SMA_response}
 
 
 def on_update(event):
     physical_id = event["PhysicalResourceId"]
     props = event["ResourceProperties"]
     print("update resource %s with props %s" % (physical_id, props))
-    return {'PhysicalResourceId': physical_id}
+    return {"PhysicalResourceId": physical_id}
 
 
 def on_delete(event):
     physical_id = event["PhysicalResourceId"]
     print("delete resource %s" % physical_id)
-    return {'PhysicalResourceId': physical_id}
+    return {"PhysicalResourceId": physical_id}
--- utils/createWav.py
+++ utils/createWav.py
@@ -4,42 +4,37 @@ import wave
 import argparse
 import sys
 
-polly = boto3.client('polly')
+polly = boto3.client("polly")
 
 
 def createPolly(pollyText, fileName):
-    response = polly.synthesize_speech(
-        OutputFormat='pcm',
-        Text=pollyText,
-        SampleRate='8000',
-        VoiceId='Joanna'
-    )
+    response = polly.synthesize_speech(OutputFormat="pcm", Text=pollyText, SampleRate="8000", VoiceId="Joanna")
 
     if "AudioStream" in response:
-        outputWav = '../wav_files/' + fileName + '.wav'
-        with wave.open(outputWav, 'wb') as wav_file:
-            wav_file.setparams((1, 2, 8000, 0, 'NONE', 'NONE'))
-            wav_file.writeframes(response['AudioStream'].read())
+        outputWav = "../wav_files/" + fileName + ".wav"
+        with wave.open(outputWav, "wb") as wav_file:
+            wav_file.setparams((1, 2, 8000, 0, "NONE", "NONE"))
+            wav_file.writeframes(response["AudioStream"].read())
 
     return outputWav
 
 
-parse_msg = 'Simple utility to create wav files for SMA via Polly'
-parser = argparse.ArgumentParser(prog='createWav.py', description=parse_msg)
-parser.add_argument('-file', help='Name of file to be created (without .wav)')
-parser.add_argument('-text', help='Text of the audio to be created in quotes')
+parse_msg = "Simple utility to create wav files for SMA via Polly"
+parser = argparse.ArgumentParser(prog="createWav.py", description=parse_msg)
+parser.add_argument("-file", help="Name of file to be created (without .wav)")
+parser.add_argument("-text", help="Text of the audio to be created in quotes")
 args = parser.parse_args()
 
 fileName = args.file
 pollyText = args.text
 
 if not fileName:
-    print('Filename is rquired')
+    print("Filename is rquired")
     sys.exit()
 
 if not pollyText:
-    print('Text is required')
+    print("Text is required")
     sys.exit()
 
 wavFile = createPolly(pollyText, fileName)
-print('wav file created: ' + wavFile)
+print("wav file created: " + wavFile)
